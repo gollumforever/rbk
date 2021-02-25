bf423ff4c5a188f5cd6a15289710281b684b37cc
Riz <rizwana.akmal@hotmail.com>
2021-02-25 23:03:05 +0000

    accessibility improvements

--- pages/footer.js
+++ pages/footer.js
@@ -7,13 +7,13 @@ export default function Footer() {
                 <p>© {new Date().getFullYear()} <span className={styles.caps}>Rizwana Akmal Khan</span></p>
                 <div className={styles.footer_icons}>
                     <div>
-                        <a href="mailto:rizwana.akmal@hotmail.com" target="_blank" rel="noopener"><img src="/icons/mail.svg"/></a>
-                        <a href="https://twitter.com/rizbizkits" target="_blank" rel="noopener"><img src="/icons/twitter.svg"/></a>
-                        <a href="https://www.linkedin.com/in/riz" target="_blank" rel="noopener"><img src="/icons/linkedin.svg"/></a>
-                        <a href="https://rizwanakhan.com/rss.xml" target="_blank" rel="noopener"><img src="/icons/rss.svg"/></a>
+                        <a href="mailto:rizwana.akmal@hotmail.com" target="_blank" rel="noopener" aria-label="Email"><img src="/icons/mail.svg" alt=""/></a>
+                        <a href="https://twitter.com/rizbizkits" target="_blank" rel="noopener" aria-label="Twitter"><img src="/icons/twitter.svg" alt=""/></a>
+                        <a href="https://www.linkedin.com/in/riz" target="_blank" rel="noopener" aria-label="LinkedIn"><img src="/icons/linkedin.svg" alt=""/></a>
+                        <a href="https://rizwanakhan.com/rss.xml" target="_blank" rel="noopener" aria-label="XML"><img src="/icons/rss.svg" alt=""/></a>
                     </div>
                     <div className={styles.footer_icons__rewind}>
-                        <a href="https://v1.rizwanakhan.com/" target="_blank" rel="noopener"><img src="/icons/skip-back.svg"/>V1</a>
+                        <a href="https://v1.rizwanakhan.com/" target="_blank" rel="noopener" aria-label="V1"><img src="/icons/skip-back.svg" alt=""/>V1</a>
                     </div>
                 </div>
 			</footer>

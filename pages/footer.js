fec8f315caa90f871646a400b7d3337e83578e97
Riz <rizwana.akmal@hotmail.com>
2021-02-25 23:30:48 +0000

    xml update

--- pages/footer.js
+++ pages/footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
                         <a href="mailto:rizwana.akmal@hotmail.com" target="_blank" rel="noopener" aria-label="Email"><img src="/icons/mail.svg" alt=""/></a>
                         <a href="https://twitter.com/rizbizkits" target="_blank" rel="noopener" aria-label="Twitter"><img src="/icons/twitter.svg" alt=""/></a>
                         <a href="https://www.linkedin.com/in/riz" target="_blank" rel="noopener" aria-label="LinkedIn"><img src="/icons/linkedin.svg" alt=""/></a>
-                        <a href="https://rizwanakhan.com/rss.xml" target="_blank" rel="noopener" aria-label="XML"><img src="/icons/rss.svg" alt=""/></a>
+                        <a href="https://v1.rizwanakhan.com/rss.xml" target="_blank" rel="noopener" aria-label="XML"><img src="/icons/rss.svg" alt=""/></a>
                     </div>
                     <div className={styles.footer_icons__rewind}>
                         <a href="https://v1.rizwanakhan.com/" target="_blank" rel="noopener" aria-label="V1"><img src="/icons/skip-back.svg" alt=""/>V1</a>

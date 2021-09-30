8aca630309c47f64aaf85be812bad809ba9d8826
Riz <rizwana.akmal@hotmail.com>
2021-09-30 16:47:19 +0100

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -1,20 +1,22 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 Projects.meta = {
-    title: "Projects",
-    description: "What I've been up to lately."
-}
+  title: "Projects",
+  description: "What I've been up to lately.",
+};
 export default function Projects() {
-    return (
-        <div className="outer">
-        <section>
-            <div className={"pt-3 pb-2"}>
-                <p className={"i fade"}>Sit tight, this page isn't ready just yet. Check back soon?</p>
-            </div>
-        </section>
-
+  return (
+    <div className="outer">
+      <section>
+        <div className={"pt-3 pb-2"}>
+          <p className={"i fade"}>
+            Sit tight, this page isn't ready just yet. Check back soon?
+          </p>
+          <p>test - 1</p>
         </div>
-    )
+      </section>
+    </div>
+  );
 }
